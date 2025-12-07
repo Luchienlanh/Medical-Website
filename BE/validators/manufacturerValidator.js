ebab5a001b4addb1f21cf0d6bc5e527bfb7bd13f
Luchienlanh <lucdo2511@gmail.com>
2025-12-07 10:52:03 +0700

    Create validator for Manuf

---
+++ BE/validators/manufacturerValidator.js
@@ -0,0 +1,28 @@
+export const createManufacturerSchema = Joi.object({
+    manufacturerName: Joi.string()
+    .required()
+    .messages({
+        'string.empty': 'Tên nhà sản xuất không được để trống!'
+    }),
+    address: Joi.string().optional(),
+    phoneNum: Joi.string()
+    .pattern(/^[0-9]{10}$/)
+    .messages({
+        'string.pattern.base': 'Số điện thoại phải có 10 chữ số!'
+    })
+})
+
+export const updateManufacturerSchema = Joi.object({
+    manufacturerName: Joi.string()
+    .optional()
+    .messages({
+        'string.empty': 'Tên nhà sản xuất không được để trống!'
+    }),
+    address: Joi.string().optional(),
+    phoneNum: Joi.string()
+    .optional()
+    .pattern(/^[0-9]{10}$/)
+    .messages({
+        'string.pattern.base': 'Số điện thoại phải có 10 chữ số!'
+    })
+})
