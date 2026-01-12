75084da64c496ce5e3e67996675eb6439771b2e8
Luchienlanh <lucdo2511@gmail.com>
2026-01-13 02:35:37 +0700

    config properties

--- BE/models/product/ProductBatch.js
+++ BE/models/product/ProductBatch.js
@@ -1,8 +1,8 @@
 import mongoose from 'mongoose'
 
 const productBatchSchema = new mongoose.Schema({
-    productId: {type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true},
-    purchaseInvoiceId: {type: mongoose.Schema.Types.ObjectId, ref: 'PurchaseInvoice', required: true},
+    productId: {type: mongoose.Schema.Types.ObjectId, ref: 'Product'},
+    purchaseInvoiceId: {type: mongoose.Schema.Types.ObjectId, ref: 'PurchaseInvoice'},
     manufactureDate: {type: Date, required: true},
     expiryDate: {type: Date, required: true},
     quantity: {type: Number, required: true},
