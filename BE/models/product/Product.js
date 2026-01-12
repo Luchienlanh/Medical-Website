8377cd9cfa97846e2bd92714cd5b730154f25cb1
Luchienlanh <lucdo2511@gmail.com>
2026-01-13 01:27:36 +0700

    change logic product

--- BE/models/product/Product.js
+++ BE/models/product/Product.js
@@ -2,8 +2,8 @@ import mongoose from 'mongoose'
 
 const productSchema = new mongoose.Schema({
     productName: { type: String, required: true },
-    manufacturerId: { type: mongoose.Schema.Types.ObjectId, ref: 'Manufacturer', required: true },
-    categoryId: { type: mongoose.Schema.Types.ObjectId, ref: 'Category', required: true },
+    manufacturerId: { type: mongoose.Schema.Types.ObjectId, ref: 'Manufacturer'},
+    categoryId: { type: mongoose.Schema.Types.ObjectId, ref: 'Category'},
     img: { type: String },
     productDesc: { type: String },
     packagingType: { type: String, enum: ['Blister', 'Box', 'Bottle', 'Tube', 'Sachet', 'Ampoule', 'Vial', 'Bag'] },
