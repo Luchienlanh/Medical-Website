0df886934c1e34b6ebc8c3e8f71142ad6577e594
Luchienlanh <lucdo2511@gmail.com>
2026-01-15 15:26:09 +0700

    add properties - description

--- BE/models/product/Category.js
+++ BE/models/product/Category.js
@@ -1,7 +1,8 @@
 import mongoose from 'mongoose'
 
 const categorySchema = new mongoose.Schema({
-    categoryName: {type: String, required: true}
+    categoryName: {type: String, required: true},
+    description: {type: String}
 }, {timestamps: true});
 
 export const Category = mongoose.model('Category', categorySchema)
