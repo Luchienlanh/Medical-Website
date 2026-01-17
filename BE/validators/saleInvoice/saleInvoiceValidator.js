04e6006fc43fe083ea46882cc571762f54d1563f
Luchienlanh <lucdo2511@gmail.com>
2026-01-18 00:45:18 +0700

    config detail invoice

--- BE/validators/saleInvoice/saleInvoiceValidator.js
+++ BE/validators/saleInvoice/saleInvoiceValidator.js
@@ -1,10 +1,19 @@
 import Joi from "joi"
+import { createSaleInvoiceDetailValidator } from "./saleInvoiceDetailValidator.js";
 
 export const createSaleInvoiceValidator = Joi.object({
     userId: Joi.string().optional(),
     saleDate: Joi.date()
     .default(() => new Date(), 'current date'),
-    statusId: Joi.string().required()
+    statusId: Joi.string().optional(),
+    details: Joi.array()
+    .items(createSaleInvoiceDetailValidator)
+    .min(1)
+    .required()
+    .messages({
+        'any.base': 'Chi tiết hóa đơn không hợp lệ!',
+        'array.min': 'Chi tiết hóa đơn phải có ít nhất 1 sản phẩm!',
+    })
 })
 
 export const updateSaleInvoiceValidator = Joi.object({
@@ -12,5 +21,13 @@ export const updateSaleInvoiceValidator = Joi.object({
     .optional()
     .messages({
         'date.base': 'Ngày bán không hợp lệ!'
+    }),
+    details: Joi.array()
+    .items(createSaleInvoiceDetailValidator)
+    .min(1)
+    .optional()
+    .messages({
+        'any.base': 'Chi tiết hóa đơn không hợp lệ!',
+        'array.min': 'Chi tiết hóa đơn phải có ít nhất 1 sản phẩm!',
     })
 })
